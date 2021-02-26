21410ec0a1b2f3ffd545b0ab90f58164794bf8aa
KAMIKU <kamiku319@gmail.com>
2021-02-25 23:49:47 -0500

    初始commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# RunJR
+
+Developed with Unreal Engine 4
---
+++ Source/RunJR/RunJRGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RunJRGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RUNJR_API ARunJRGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
